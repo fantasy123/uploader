dff2580fade651c06b7d26c448b2c7ea6ebfdff1
BYSJ <bourgeois_123@163.com>
2016-11-27 11:11:39 +0800

    touch_slide_commit

---
+++ touch_slide/js/data.js
@@ -0,0 +1,37 @@
+var list = [//初始化DOM需要的数据结构
+	{
+		height: 950,//图片的宽高无需单位
+		width: 800,
+		src: "imgs/1.jpg"
+	},
+	{
+		height: 1187,
+		width: 900,
+		src: "imgs/2.jpg"
+	},
+	{
+		height: 766,
+		width: 980,
+		src: "imgs/3.jpg"
+	},
+	{
+		height: 754,
+		width: 980,
+		src: "imgs/4.jpg"
+	},
+	{
+		height: 493,
+		src: "imgs/5.jpg",
+		width: 750
+	},
+	{
+		height: 500,
+		src: "imgs/6.jpg",
+		width: 750
+	},
+	{	
+		height: 600,
+		src: "imgs/7.jpg",
+		width: 400
+	}
+];
